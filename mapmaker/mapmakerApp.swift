1073a8d66c5ef60af155f10d4bddce978a44c419
conradbooker <conrad.booker@gmail.com>
2023-04-17 13:14:38 -0400

    Initial Commit

---
+++ mapmaker/mapmakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapmakerApp.swift
+//  mapmaker
+//
+//  Created by Conrad on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct mapmakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
